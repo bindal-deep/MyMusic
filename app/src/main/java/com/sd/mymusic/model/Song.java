95f705137586a9e5384f13134419c6dd85e82239
bindal <bindal@carmatec.in>
2018-07-11 15:21:42 +0530

    first commit

---
+++ app/src/main/java/com/sd/mymusic/model/Song.java
@@ -0,0 +1,29 @@
+package com.sd.mymusic.model;
+
+/**
+ * Created by bindal on 9/7/18.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+    }
+
+    public long getID() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
